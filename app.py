de760bc77cc62c532c66077f992e495cf2ae6666
HarnaiDigital <harnaiteck@gmail.com>
2026-06-04 12:40:01 +0500

    Update

--- app.py
+++ app.py
@@ -1233,4 +1233,5 @@ def download_file(filename):
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
